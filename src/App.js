ec7b383c6c9ab91ff0d442e8efb819d3d4666dd2
raajnadar <raajnadar@gmail.com>
2018-10-30 19:29:27 +0530

    add: color selection mechanism

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogActions from '@material-ui/core/DialogActions'
+import Dialog from '@material-ui/core/Dialog'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import Radio from '@material-ui/core/Radio'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
 
 import { withStyles } from '@material-ui/core/styles'
 
 const styles = theme => ({
 	root: {
 		flexGrow: 1,
-		padding: '20px'
+		padding: '48px'
 	},
 	buttonContainer: {
 		margin: '10px 0px',
@@ -22,8 +29,72 @@ const styles = theme => ({
 	}
 })
 
+const colors = [
+	'black',
+	'brown',
+	'red',
+	'orange',
+	'yellow',
+	'green',
+	'blue',
+	'violet',
+	'grey',
+	'white'
+]
+
+const tolerance = ['Gold', 'Silver', 'Transparent']
+
 function App(props) {
 	const { classes } = props
+
+	const [color, setColor] = useState({
+		first: 'First color',
+		second: 'Second color',
+		third: 'Third color',
+		fourth: 'Fourth color'
+	})
+
+	const [dialog, setDialog] = useState(false)
+	const [content, setDialogContent] = useState(colors)
+	const [activeDialog, setActiveDialog] = useState('')
+	const buttonClick = name => {
+		if (name === 'fourth') {
+			setDialogContent(tolerance)
+		} else {
+			setDialogContent(colors)
+		}
+		setDialog(true)
+		setActiveDialog(name)
+	}
+
+	function handleCancel() {
+		setDialog(false)
+		setColor({
+			first: 'First color',
+			second: 'Second color',
+			third: 'Third color',
+			fourth: 'Fourth color'
+		})
+	}
+
+	function handleOk() {
+		setDialog(false)
+	}
+
+	const handleChange = (e, value) => {
+		let newColor = color
+
+		newColor[activeDialog] = value
+		if (activeDialog === 'first') {
+			setColor(newColor)
+		} else if (activeDialog === 'second') {
+			setColor(newColor)
+		} else if (activeDialog === 'third') {
+			setColor(newColor)
+		} else {
+			setColor(newColor)
+		}
+	}
 
 	return (
 		<div className={classes.root}>
@@ -80,29 +151,33 @@ function App(props) {
 								<Grid item lg={3} sm={6} xs={12}>
 									<Button
 										className={classes.button}
-										variant="contained">
-										First Color
+										variant="contained"
+										onClick={() => buttonClick('first')}>
+										{color.first}
 									</Button>
 								</Grid>
 								<Grid item lg={3} sm={6} xs={12}>
 									<Button
 										className={classes.button}
-										variant="contained">
-										Second Color
+										variant="contained"
+										onClick={() => buttonClick('second')}>
+										{color.second}
 									</Button>
 								</Grid>
 								<Grid item lg={3} sm={6} xs={12}>
 									<Button
 										className={classes.button}
-										variant="contained">
-										Third Color
+										variant="contained"
+										onClick={() => buttonClick('third')}>
+										{color.third}
 									</Button>
 								</Grid>
 								<Grid item lg={3} sm={6} xs={12}>
 									<Button
 										className={classes.button}
-										variant="contained">
-										Fourth Color
+										variant="contained"
+										onClick={() => buttonClick('fourth')}>
+										{color.fourth}
 									</Button>
 								</Grid>
 							</Grid>
@@ -185,6 +260,42 @@ function App(props) {
 					</Grid>
 				</Grid>
 			</Grid>
+
+			<Dialog
+				open={dialog}
+				disableBackdropClick
+				disableEscapeKeyDown
+				fullWidth={true}
+				maxWidth="md"
+				aria-labelledby="confirmation-dialog-title">
+				<DialogTitle id="confirmation-dialog-title">
+					Phone Ringtone
+				</DialogTitle>
+				<DialogContent>
+					<RadioGroup
+						aria-label="Ringtone"
+						name="ringtone"
+						value={color[activeDialog]}
+						onChange={handleChange}>
+						{content.map(color => (
+							<FormControlLabel
+								value={color}
+								key={color}
+								control={<Radio />}
+								label={color}
+							/>
+						))}
+					</RadioGroup>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleCancel} color="primary">
+						Cancel
+					</Button>
+					<Button onClick={handleOk} color="primary">
+						Ok
+					</Button>
+				</DialogActions>
+			</Dialog>
 		</div>
 	)
 }
