db2552e88678d3d9aacf40f930b267081cb539f4
raajnadar <raajnadar@gmail.com>
2018-11-07 18:35:19 +0530

    Update: decimal converter

--- src/functions/Calculator.js
+++ src/functions/Calculator.js
@@ -8,6 +8,17 @@ export default function Calculator(color, type) {
 	} else {
 		value = `${ColorIndex(color.first)}${ColorIndex(color.second)}`
 		value = value * 10 ** ColorIndex(color.third)
+
+		if (value >= 1000 && value <= 999999) {
+			value = value / 1000
+			value = value + ' K'
+		} else if (value >= 1000000 && value <= 999999999) {
+			value = value / 1000000
+			value = value + ' M'
+		} else if (value >= 1000000000 && value <= 99999999999) {
+			value = value / 1000000000
+			value = value + ' G'
+		}
 	}
 
 	return value
