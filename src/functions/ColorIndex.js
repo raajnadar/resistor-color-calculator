2cf61b3a529004d61b4efe0c54b7766e4ad78ed2
raajnadar <raajnadar@gmail.com>
2018-11-08 16:58:11 +0530

    Update: add more tolerance value

--- src/functions/ColorIndex.js
+++ src/functions/ColorIndex.js
@@ -1,6 +1,22 @@
 export default function ColorIndex(color, type) {
 	if (type === 'tolerance') {
 		switch (color) {
+			case 'Brown':
+				return 1
+			case 'Red':
+				return 2
+			case 'Orange':
+				return 3
+			case 'Yellow':
+				return 4
+			case 'Green':
+				return 0.5
+			case 'Blue':
+				return 0.25
+			case 'Violet':
+				return 0.1
+			case 'Gray':
+				return 0.05
 			case 'Gold':
 				return 5
 			case 'Silver':
--- src/list/Tolerance.js
+++ src/list/Tolerance.js
@@ -1,3 +1,15 @@
-const Tolerance = ['Gold', 'Silver', 'Transparent']
+const Tolerance = [
+	'Brown',
+	'Red',
+	'Orange',
+	'Yellow',
+	'Green',
+	'Blue',
+	'Violet',
+	'Gray',
+	'Gold',
+	'Silver',
+	'Transparent'
+]
 
 export default Tolerance
