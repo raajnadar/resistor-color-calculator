8232a5f0ed2aa2c24b2ca6e29ee80c1cd34816c7
raajnadar <raajnadar@gmail.com>
2018-11-19 22:24:08 +0530

    Updates

---
+++ precache-manifest.f70679111c22202bd5ce1a7ae3684a98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29dfe2b40fef75de9db1",
+    "url": "/resistor-color-calculator/static/js/runtime~main.29dfe2b4.js"
+  },
+  {
+    "revision": "02ea11039e806237186a",
+    "url": "/resistor-color-calculator/static/js/main.02ea1103.chunk.js"
+  },
+  {
+    "revision": "d05edd70a6ed30d13525",
+    "url": "/resistor-color-calculator/static/js/1.d05edd70.chunk.js"
+  },
+  {
+    "revision": "02ea11039e806237186a",
+    "url": "/resistor-color-calculator/static/css/main.f4f3a251.chunk.css"
+  },
+  {
+    "revision": "69130a60d11e46b4a41804b6e0d84394",
+    "url": "/resistor-color-calculator/index.html"
+  }
+];
