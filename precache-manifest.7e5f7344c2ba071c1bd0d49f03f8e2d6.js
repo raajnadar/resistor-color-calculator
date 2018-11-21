972641e246626eb7829151b2b6d54d53e5efce78
raajnadar <raajnadar@gmail.com>
2018-11-21 14:54:50 +0530

    Updates

--- precache-manifest.7e5f7344c2ba071c1bd0d49f03f8e2d6.js
+++ precache-manifest.7e5f7344c2ba071c1bd0d49f03f8e2d6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/resistor-color-calculator/static/js/runtime~main.29dfe2b4.js"
   },
   {
-    "revision": "a904e43024768692449d",
-    "url": "/resistor-color-calculator/static/js/main.a904e430.chunk.js"
+    "revision": "ef4b0e27ef85904d19a9",
+    "url": "/resistor-color-calculator/static/js/main.ef4b0e27.chunk.js"
   },
   {
-    "revision": "3eaf3bc5b1d9e5e89efb",
-    "url": "/resistor-color-calculator/static/js/1.3eaf3bc5.chunk.js"
+    "revision": "15c99c21dcad3210cdcf",
+    "url": "/resistor-color-calculator/static/js/1.15c99c21.chunk.js"
   },
   {
-    "revision": "a904e43024768692449d",
+    "revision": "ef4b0e27ef85904d19a9",
     "url": "/resistor-color-calculator/static/css/main.12df41f4.chunk.css"
   },
   {
-    "revision": "a4b561f325951a8723e063bdb9b598c7",
+    "revision": "cc2b582d568f4d8b68a16580810278f4",
     "url": "/resistor-color-calculator/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/resistor-color-calculator/precache-manifest.143cdde3d4922cdcbc24ce469a7c7eea.js"
+  "/resistor-color-calculator/precache-manifest.7e5f7344c2ba071c1bd0d49f03f8e2d6.js"
 );
 
 workbox.clientsClaim();
