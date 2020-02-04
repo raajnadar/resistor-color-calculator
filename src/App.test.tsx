b1a4eaa8387a584df67043ab5f0574868d03d449
raajnadar <raajnadar@gmail.com>
2020-02-04 20:02:45 +0530

    chore: add first unit test

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import App from './App'
+
+test('Application renders without crash', () => {
+	render(<App />)
+	return undefined
+})
